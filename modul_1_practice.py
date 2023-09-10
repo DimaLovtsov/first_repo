a6c12141bb75ff3ba26b881667f8018f3873cab0
DimaLovtsov <dmytrolovtsov@gmail.com>
2023-09-10 20:09:48 +0200

    add modul 2

--- modul_1_practice.py
+++ modul_1_practice.py
@@ -10,16 +10,22 @@
 # z = x + y
 # print(z)
 
-import math
+# import math
 
-RADIUS = 6371.01
+# RADIUS = 6371.01
 
-lat1 = 50.45
-lon1 = 30.523
+# lat1 = 50.45
+# lon1 = 30.523
 
-lat2 = 51.5072
-lon2 = -0.1275
+# lat2 = 51.5072
+# lon2 = -0.1275
 
-distance = RADIUS * math.acos(math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(lon1) - math.radians(lon2)))
-print(distance)
+# distance = RADIUS * math.acos(math.sin(math.radians(lat1)) * math.sin(math.radians(lat2)) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(lon1) - math.radians(lon2)))
+# print(distance)
 
+user_name = input("Enter your name: ")
+
+if user_name:
+    print(f"Hello {user_name}")
+else:
+    print("Hi Anonym!")
