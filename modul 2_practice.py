4cf6cd04adbfc029949812c4b649e91855238cdf
DimaLovtsov <dmytrolovtsov@gmail.com>
2023-09-12 19:32:54 +0200

    condit 1

---
+++ modul 2_practice.py
@@ -0,0 +1,27 @@
+# name = "Iryna"
+# name = "Petro"
+
+# if name == "Iryna":
+#     print("Hello Iryna")
+# else:
+#     print(f"Hi {name}")
+
+'''
+a = 50
+
+if a >= 50:
+    print("a>= 50")
+if a < 100:
+   print("a < 50") 
+if a <= 50:
+    print("a <= 50")
+'''
+
+a = int(input("Your number: "))
+
+if a >= 50:
+    print("a>= 50")
+elif a < 100:
+   print("a < 50") 
+elif a <= 50:
+    print("a <= 50")
