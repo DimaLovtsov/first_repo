43c29cd99cf0d4354b84901ea91da2c0f541ea66
DimaLovtsov <dmytrolovtsov@gmail.com>
2023-09-12 20:23:53 +0200

    for in

--- modul 2_practice.py
+++ modul 2_practice.py
@@ -6,22 +6,36 @@
 # else:
 #     print(f"Hi {name}")
 
-'''
-a = 50
+# 
 
-if a >= 50:
-    print("a>= 50")
-if a < 100:
-   print("a < 50") 
-if a <= 50:
-    print("a <= 50")
-'''
+# count = 1
+# while count <= 5:
+#     print(f"count: {count}")
+#     count += 1
 
-a = int(input("Your number: "))
 
-if a >= 50:
-    print("a>= 50")
-elif a < 100:
-   print("a < 50") 
-elif a <= 50:
-    print("a <= 50")
+# while True:
+#     s = input("Please enter str [q - to quit]: ")
+#     print(s.capitalize())
+#     if s == "q":
+#         break
+
+# print("Thank you")
+
+# '''
+# while True:
+#     print("Hello!")
+#     name = input("Please enter your name: ") 
+#     if name != "Vadym":
+#         continue
+#     print("Hello, Vadym. How old are you?")
+#     age = input()
+#     if age == 18:
+#         print("Cool")   #problem with cool
+#         break
+# '''
+
+print("Hello")
+item = input("Enter a number: ")
+for item in range(5, 21, 2)
+    print(f'Iteration (item)')
