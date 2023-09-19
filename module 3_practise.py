ff0907219994e9df8672844259dc73f7f55193b2
DimaLovtsov <dmytrolovtsov@gmail.com>
2023-09-19 16:10:52 +0200

    M3 Ex 4

--- module 3_practise.py
+++ module 3_practise.py
@@ -29,9 +29,78 @@
 
 # invite_to_event("Vasya")
 
-def invite_to_event(username):
+# def invite_to_event(username):
 
-    name_invit = f"Dear {username}, we have the honour to invite you to our event"
-    return name_invit
+#     name_invit = f"Dear {username}, we have the honour to invite you to our event"
+#     return name_invit
 
-print(invite_to_event("username"))
+# print(invite_to_event("username"))
+
+
+
+# Ex 3
+
+
+
+# a = 5
+# b = 0
+
+
+# def fun():
+#     global a
+#     a = 10
+#     b = 2
+
+
+# fun()
+# print(a) 
+# print(b)  
+
+
+
+# base_rate = 40
+# price_per_km = 10
+# total_trip = 0
+
+
+# def calculate_trip_price(distance_km):
+
+#     global total_trip
+#     total_trip += 1
+
+#     return base_rate + price_per_km * distance_km
+
+# result = calculate_trip_price(10)
+# print(result)
+
+
+# Ex 4
+
+# def func_outer():
+#     x = 2
+
+#     def func_inner():
+#         nonlocal x
+#         x = 5
+
+#     func_inner()
+#     return x
+
+
+# result = func_outer()
+# print(result)
+
+
+def discount_price(price, discount):
+    def apply_discount():
+        
+        nonlocal price
+        discount = price/int("100")
+        price -= discount
+
+    apply_discount()
+    return price
+
+
+result = discount_price(10, 1)
+print(result)
---
+++ module 3_theory.py
@@ -0,0 +1,51 @@
+# def print_hello():
+#     print("Hello")
+
+
+# def calculate_sum():
+
+#     print("Calculation")
+
+#     x = 3
+#     y = 5
+
+#     return x + y
+
+# x = 2
+# print(x)   # name 'x'is not definded  Есть х (стр 9) , но он изолирован внутри функции (стр 5) и не выходит за ее пределы. Сначала интерпретатор
+            # читает стр 1, внутрь не заходит. Потом стр 5, внутрь не заходит и не подозревает, что есть какой-то х, пока мы не вызовем функцию.
+            # а если мы допишем стр 14, то будет на экране результат со стр 14.
+
+# global существует, но стараемся не использовать его внутри local
+# 
+# 
+
+'''
+def calculate_sum():
+
+    x = 3
+    y = 5
+
+    def inner_func():
+        print(x)
+        # x = 10
+        print(x)
+
+    inner_func()
+
+    return x + y
+
+calculate_sum()
+'''
+
+def calculate_sum(x):
+    
+    y = 3
+
+    return x + y
+
+calculate_sum(5)   #TypeError: calculate_sum() missing 1 required positional argument: 'x'  
+                    # Нужно обязательно при вызове указать в скобках аргумент, но не х, а его значение, например 5. Правильный вариант ниже.
+
+result = calculate_sum(5)
+print(result)
