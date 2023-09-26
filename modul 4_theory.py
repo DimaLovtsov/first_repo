4282c47a440a3e1214d1590487b848c95deefd75
DimaLovtsov <dmytrolovtsov@gmail.com>
2023-09-26 16:57:29 +0200

    ex 3

--- modul 4_theory.py
+++ modul 4_theory.py
@@ -1,3 +1,29 @@
-num = [1, 2, 41, 3, 5, 41, 8]
-num.sort()
-print(num)
+# num = [1, 2, 41, 3, 5, 41, 8]
+# num.sort()
+# print(num)
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+odd_numbers = numbers[0:10:2]
+even_numbers = numbers[1:10:2]
+three_numbers = numbers[1:10:3]
+
+print(odd_numbers)
+print(even_numbers)
+print(three_numbers)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+odd_numbers = numbers[::2]
+even_numbers = numbers[1::2]
+three_numbers = numbers[1:10:3]
+numbers_copy = numbers[:]
+
+print(odd_numbers)
+print(even_numbers)
+print(numbers_copy)
+
+
+items_a = ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]
+items_b = items_a[0, 1, 2]
--- module 4_practice.py
+++ module 4_practice.py
@@ -78,15 +78,52 @@
 
 # print(prepare_data(1))
 
-def prepare_data(data):
+# def prepare_data(data):
 
-    value_list = [5, 27, 269, 8, 1, -126, 12, 22, 18]
-    value_list.sort()
-    print(value_list)
+#     value_list = [5, 27, 269, 8, 1, -126, 12, 22, 18]
+#     value_list.sort()
+#     print(value_list)
 
-    first = value_list.pop(0)
-    second = value_list.pop()
+#     first = value_list.pop(0)
+#     second = value_list.pop()
 
-    return value_list
+#     return value_list
 
-print(prepare_data(1))
+# print(prepare_data(1))
+
+# def prepare_data(data):
+
+#     data = []
+
+#     first = data.pop(0)
+#     second = data.pop()
+
+#     return data
+
+# print(prepare_data(1))
+
+#правильне рішення завдання 2
+
+# def prepare_data(data):
+
+#     data.remove(min(data))
+#     data.remove(max(data))
+#     new_data = sorted(data)
+#     return new_data
+
+# data = [5, 27, 269, 8, 1, -126, 12, 22, 18]
+# print(prepare_data(data))
+
+
+# Ex 3. Ми розробляємо кулінарний блог. І в рецептах, при написанні списку інгредієнтів, ми розділяємо їх комами, а перед останнім ставимо союз "and", як у прикладі нижче:
+
+# 2 eggs, 1 liter sugar, 1 tsp salt and vinegar
+# Напишіть функцію format_ingredients, яка прийматиме на вхід список з інгредієнтів ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"] та повертатиме рядок зібраний з його елементів в описаний вище спосіб. Ваша функція має вміти обробляти списки будь-якої довжини.
+
+def format_ingredients(items):
+    
+    items_a = ["2 eggs", "1 liter sugar", "1 tsp salt", "vinegar"]
+    a = items_a.insert(3, "and")
+    return(items_a)
+
+print(format_ingredients(1))
