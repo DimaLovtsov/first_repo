0a8e15b3b68a66fe01f9017818e376025e842d8e
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-05-25 11:09:40 +0200

    revision_25_05_v2

--- self-education/revision_24_04_25_05.py
+++ self-education/revision_24_04_25_05.py
@@ -1,2 +1,62 @@
 print(int(True))
 print(int(False))
+
+#float to integer
+'''
+print(int(3.06))
+a = 5.52
+print(type(a))
+b = 4.03
+print(type(b))
+c = a + b
+print(c)
+print(type(c))
+d = int(a + b)
+print(d)
+print(type(d))
+
+e = -5.7
+print(type(e))
+f = -10.1
+print(type(f))
+g = e + f
+print(g)
+print(type(g))
+print(int(g))
+
+'''
+'''
+
+#string with numbers to integer
+
+a = '230'
+print(type(a))
+
+b = '120'
+print(type(b))
+
+c = a + ' ' + b
+d = a + b
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+
+e = int(a+b)
+print(e)
+print(type(e))
+
+f = int(a)
+g = int(b)
+
+h = f + g
+
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+
+'''
+
