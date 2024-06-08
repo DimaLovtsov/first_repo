8f9852552ccbdff82957ecbefaa6ff0ca548a61f
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-08 15:10:12 +0200

    recursive_exercice_1

--- self-education/15_function_recursive.py
+++ self-education/15_function_recursive.py
@@ -1,2 +1,13 @@
 #Exercice 1
-#
+#Factorial
+
+def factorial(n):
+
+    if n == 1:
+        return 1
+    else:
+        return (n * factorial(n - 1))
+    
+num = int(input("Enter an integer: "))
+
+print("The factotial of", num, "is", factorial(num))
