8dbd7d157d38b3e515a365f205a8c9ed14323786
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-09 16:33:54 +0200

    adding module invite

--- self-education/23_moduls.py
+++ self-education/23_moduls.py
@@ -1,3 +1,14 @@
 #Exercice 1
 #Create a module "module_invite" and save it as a modul (file "module_invite")
+#import the module "module_invite"
+
+import module_invite
+
+one = input("Enter the person one: ")
+two = input("Enter the person two: ")
+
+result = module_invite.invite(one, two)
+
+print(result)
+
 
--- self-education/module_invite.py
+++ self-education/module_invite.py
@@ -3,7 +3,3 @@ def invite(guest_1, guest_2):
     y = "We invite you!"
     return x + " " + y
     
-one = input("Enter the person one: ")
-two = input("Enter the person two: ")
-
-print(invite(one, two))
