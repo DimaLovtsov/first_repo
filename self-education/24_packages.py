37f475be3742c6e3b3c85cdfde3c0721c75baf56
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-11 09:59:57 +0200

    packages game

--- self-education/24_packages.py
+++ self-education/24_packages.py
@@ -1,4 +1,4 @@
-#Create packages: Dota 2 (Main), Characters, Creeps, Items, Runes
+#Create a package Dota 2 (Main) which contains packages Characters, Creeps, Items, Runes
 #In Characters create AA, Warlock, Techies
 #In Creeps create lane_creeps, neutral_creeps
 #In Items create ward, clarity, tango, tp
