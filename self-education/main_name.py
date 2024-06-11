8d6d1b945ff09b39d64ecf31a6d9b19c94b8243c
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-11 20:02:41 +0200

    main_name

---
+++ self-education/main_name.py
@@ -0,0 +1,7 @@
+#print(__name__)
+
+def the_end():
+    print("It's time to rest")
+
+if __name__ == "__main__":
+    the_end()
---
+++ self-education/main_name_second.py
@@ -0,0 +1,3 @@
+import main_name #nothing
+
+main_name.the_end() #It's time to rest
