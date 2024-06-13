25a384174148fb4bd1a29b57657d74b497760688
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-13 19:55:31 +0200

    13_06_2024

---
+++ self-education/27_directories.py
@@ -0,0 +1,7 @@
+import os
+
+print(os.getcwd()) #C:\Projects VS Code\repository\first_repo
+
+os.chdir("c:/Projects VS Code/repository/first_repo/Dota2")
+
+print(os.getcwd())
---
+++ self-education/Directory1/test_dir1.py
@@ -0,0 +1,9 @@
+
+
+import os
+
+print("The CWD is: ", os.getcwd()) #C:\Projects VS Code\repository\first_repo
+
+os.chdir("C:\Projects VS Code\repository\first_repo\self_education")
+
+print("The new CWD is: ", os.getcwd())
