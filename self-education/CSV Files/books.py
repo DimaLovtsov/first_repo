89baf2bcb5c028609d678ba0197c0d89cd329689
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-16 13:41:19 +0200

    CSV v3

--- self-education/CSV Files/books.py
+++ self-education/CSV Files/books.py
@@ -1,4 +1,4 @@
-# import csv
+import csv
 
 # with open("books.csv", "w", newline= " ") as file:
 #     writer = csv.writer(file)
@@ -6,9 +6,7 @@
 #     writer.writerow([1, "Lord of the Rings", "Frodo Baggins"])
 #     writer.writerow([2, "Harry Potter", "The Moon"])
 
-import csv
-with open('protagonist.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["SN", "Movie", "Protagonist"])
-    writer.writerow([1, "Lord of the Rings", "Frodo Baggins"])
-    writer.writerow([2, "Harry Potter", "Harry Potter"])
+with open ("people.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
