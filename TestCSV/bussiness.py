45c9c962c7132fecce701fc641462c06890bde69
DimaLovtsov <dmytrolovtsov@gmail.com>
2024-06-23 14:08:50 +0200

    CSV_v8

---
+++ TestCSV/bussiness.py
@@ -0,0 +1,17 @@
+import csv
+
+try:
+    with open ('machine_2024.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, quoting=csv.QUOTE_NONE, escapechar='|')
+        line_to_read = int(input("Enter a number of lines you want to read: "))
+        current_line = 0
+            
+        for row in csv_reader:
+            print(row[:5])
+            current_line += 1
+            if current_line > line_to_read:
+                break
+
+except FileNotFoundError:
+    print("File not found. Please check the path and filename.")
+            
